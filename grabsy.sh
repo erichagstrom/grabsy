d3e8c0e576806557f691371aeb0b34c374a5edf3
Eric Hagstrom <eric.hagstrom@gotdedupe.com>
2016-02-18 11:27:52 -0600

    Update grabsy.sh
    Updates to include query for directory

--- grabsy.sh
+++ grabsy.sh
@@ -2,29 +2,20 @@
 # grabsy.sh - # Change the default destination folder for os x screenshots.
 # 
 # USAGE
-# sh grabsy.sh <DIRNAME> 
-#       to make that the default destination for screenshots
-# sh grabsy.sh
-#       to revert to your default destination.  I didn't wanteit on the ~/Desktop, but you might.  Edit the config below.
-# 
-# you can edit your path so you dont have to type sh or ./ or whatever.
-# I tested this in bash on os x mavericks.  it probably won't work anywhere else.
-# 
-# Cobbled together by Amos Brown.
+# sh grabsy.sh  
+#       You will be prompted for the destination directory for screenshots
+# sh grabsy.sh default
+#       to revert to the system default destination on the Desktop.  
+#
+# Reassembled by Eric Hagstrom.
 
-#DEFAULT_DIR=~/Desktop
-DEFAULT_DIR=~/Documents/Screenshots
-CHANGE_TO_DIR=$1
+DEFAULT_DIR=~/Desktop
+echo -n 'Enter destination directory for native OSX screenshots'
+read DIR
 
-DIR_VAR=$DEFAULT_DIR
+[ $1 == 'default' ] && DIR_VAR=$DEFAULT_DIR || DIR_VAR=$DIR
 
-echo "Changing screenshot directory to $CHANGE_TO_DIR"
-
-DIR_VAR=$DEFAULT_DIR"/"$CHANGE_TO_DIR
-mkdir -p $DIR_VAR
-echo "$DIR_VAR"
+echo "Changing screenshot destination directory to $DIR_VAR"
 
 defaults write com.apple.screencapture location $DIR_VAR
 killall SystemUIServer
-
-
